91a44db76dae6cbd731bb56be6dde8a17f012f5d
holgergp <holgergp@gmail.com>
2014-10-12 21:29:46 +0200

    Basic Bindings working

--- app/config.js
+++ app/config.js
@@ -1,31 +1,34 @@
 require.config({
-  // make bower_components more sensible
-  // expose jquery 
-  paths: {
-    "bower_components": "../bower_components",
-    "jquery": "../bower_components/jquery/jquery"
-  },
-  map: {
-    "*": {
-      "knockout": "../bower_components/knockout.js/knockout",
-      "ko": "../bower_components/knockout.js/knockout"
+    // make bower_components more sensible
+    // expose jquery
+    paths: {
+        "bower_components": "../bower_components",
+        "jquery": "../bower_components/jquery/dist/jquery",
+        "moment": "../bower_components/moment/moment"
+    },
+    map: {
+        "*": {
+            "knockout": "../bower_components/knockout.js/knockout",
+            "ko": "../bower_components/knockout.js/knockout",
+
+        }
     }
-  }
 });
 
 // Use the debug version of knockout it development only
 // When compiling with grunt require js will only look at the first 
 // require.config({}) found in this file
 require.config({
-  map: {
-    "*": {
-      "knockout": "../bower_components/knockout.js/knockout-2.3.0.debug",
-      "ko": "../bower_components/knockout.js/knockout-2.3.0.debug"
+    map: {
+        "*": {
+            "knockout": "../bower_components/knockout.js/knockout-2.3.0.debug",
+            "ko": "../bower_components/knockout.js/knockout-2.3.0.debug"
+        }
     }
-  }
 });
 
 if (!window.requireTestMode) {
-  require(['main'], function(){ });
+    require(['main'], function () {
+    });
 }
 
--- app/main.js
+++ app/main.js
@@ -1,9 +1,174 @@
 // to depend on a bower installed component:
 // define(['component/componentName/file'])
 
-define(["jquery", "knockout"], function($, ko) {
-  var viewModel = {
-    status: ko.observable('active')
-  };
-  ko.applyBindings(viewModel, $('html')[0]);
-});
+define(["jquery", "knockout", "moment"], function ($, ko, moment) {
+
+    function NewConcert() {
+
+        var self = this;
+
+
+        self.artist = ko.observable();
+        self.venue = ko.observable();
+        self.date = ko.observable(moment());
+        self.formattedDate = ko.computed(function () {
+            moment(self.date).format('DD.MM.YYYY');
+        });
+    }
+
+
+    function LoveConcerts() {
+
+        var self = this;
+
+        self.artist = 'The Saddest Landscape';
+        self.venue = 'Turock';
+        self.date = moment();
+        self.formattedDate = ko.computed(function () {
+            "use strict";
+            moment(self.date).format('DD.MM.YYYY');
+        });
+
+
+        self.loveConcerts = ko.observableArray([
+            {
+                artist: self.artist,
+                venue: self.venue,
+                date: self.date,
+                formattedDate: self.formattedDate
+            }
+
+        ])
+        ;
+
+
+    }
+
+    function AllConcerts() {
+
+        var self = this;
+
+        self.artist = 'Blind Guardian';
+        self.venue = 'Philipshalle';
+        self.date = moment();
+        self.formattedDate = ko.computed(function () {
+            "use strict";
+            moment(self.date).format('DD.MM.YYYY');
+        });
+
+
+        self.allConcerts = ko.observableArray([
+            {
+                artist: self.artist,
+                venue: self.venue,
+                date: self.date,
+                formattedDate: self.formattedDate
+            }
+
+        ])
+        ;
+
+
+    }
+
+    function MaybeConcerts() {
+
+        var self = this;
+
+        self.artist = 'Spaceman Spiff';
+        self.venue = 'FFT';
+        self.date = moment();
+        self.formattedDate = ko.computed(function () {
+            "use strict";
+            moment(self.date).format('DD.MM.YYYY');
+        });
+
+
+        self.maybeConcerts = ko.observableArray([
+            {
+                artist: self.artist,
+                venue: self.venue,
+                date: self.date,
+                formattedDate: self.formattedDate
+            }
+
+        ])
+        ;
+
+
+    }
+
+
+    function ViewModel() {
+
+        var self = this;
+
+        self.addConcert = function () {
+            self.allConcerts.push(newConcert);
+            self.newConcert = {
+                artist: '',
+                venue: '',
+                date: moment().format('L')
+            };
+        };
+
+
+        self.removeConcert = function (array, concert) {
+
+            var i = array.indexOf(concert);
+            if (i !== -1) {
+                array.splice(i, 1);
+            }
+
+        };
+
+        self.alertClass = function (concert) {
+            if (concertDueDateService.isConcertAboutToHappen(concert)) {
+                return 'bg-warning';
+            }
+            else if (concertDueDateService.isConcertOverdue(concert)) {
+                return 'bg-danger';
+            }
+            else {
+                return 'bg-primary';
+            }
+
+        };
+
+        self.formatDate = function (date) {
+            return concertDueDateService.formatDate(date);
+        };
+
+        self.status = ko.observable('active');
+    }
+
+    ko.applyBindings(new LoveConcerts(), $('#loveConcertSection')[0]);
+    ko.applyBindings(new AllConcerts(), $('#allConcertSection')[0]);
+    ko.applyBindings(new MaybeConcerts(), $('#maybeConcertSection')[0]);
+
+//ko.applyBindings(new ViewModel(), $('html')[0]);
+    ko.applyBindings(new NewConcert(), $('#newConcertSection')[0]);
+
+    /*
+     * read-only date display with momentjs
+     * use like this: data-bind="moment: dateVar, format: 'YYYY-MM-DD'"
+     * The "format" is optional and will default to "MM/DD/YYYY"
+     */
+    ko.bindingHandlers.moment = {
+        update: function (element, valueAccessor, allBindingsAccessor, viewModel) {
+            var val = valueAccessor();
+
+            var formatted = '**INVALID**'; // throw instead?
+            var date = moment(ko.utils.unwrapObservable(val));
+
+            var format = allBindingsAccessor().format || 'MM.DD.YYYY';
+
+            if (date && date.isValid()) {
+                formatted = date.format(format);
+            }
+
+            element.innerText = formatted;
+        }
+    };
+})
+;
